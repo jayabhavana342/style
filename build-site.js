faaef28fe9d3b27b2ce6b61e6f561087f9271833
Hrag Chanchanian <hchanchanian@23andme.com>
2018-06-21 15:32:34 -0700

    Button component imported and interactable

--- build-site.js
+++ build-site.js
@@ -1,5 +1,8 @@
 const nunjucks = require('nunjucks');
 
 nunjucks.configure('', { autoescape: true });
-const html = nunjucks.render('src/styleguide/site.html', { });
+const html = nunjucks.render('src/styleguide/site.html', {
+    'components_path': '../components/',
+    'site_path': '../styleguide/'
+});
 console.log(html);
