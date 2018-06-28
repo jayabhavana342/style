56af87d95204e749793b42602c730b7e885d8071
Hrag Chanchanian <hchanchanian@23andme.com>
2018-06-27 17:07:33 -0700

    Adding site assets

--- site/js/index.js
+++ site/js/index.js
@@ -1,9 +1,25 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(factory());
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (factory());
 }(this, (function () { 'use strict';
 
-	console.log('hello mars');
+    const body = document.getElementsByTagName('body')[0];
+    const headerBurg = document.querySelector('.header-burg');
+    const mobileNav = document.querySelector('.nav');
+    const closeMobileNav = document.querySelector('.nav-close');
+    const contents = document.querySelector('.contents-wrapper');
+
+    headerBurg.addEventListener('click', () => {
+        mobileNav.classList.add('mod-showing');
+        // contents.classList.add('mod-moved');
+        body.classList.add('noscroll');
+    });
+
+    closeMobileNav.addEventListener('click', () => {
+        mobileNav.classList.remove('mod-showing');
+        // contents.classList.remove('mod-moved');
+        body.classList.remove('noscroll');
+    });
 
 })));
