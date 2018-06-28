6b9086bdd3204f4df8cb84e6cb27af4f4fec482a
Hrag Chanchanian <hchanchanian@23andme.com>
2018-06-27 17:07:01 -0700

    WIP

--- src/styleguide/assets/js/index.js
+++ src/styleguide/assets/js/index.js
@@ -1,1 +1,17 @@
-console.log('hello mars');
+const body = document.getElementsByTagName('body')[0];
+const headerBurg = document.querySelector('.header-burg');
+const mobileNav = document.querySelector('.nav');
+const closeMobileNav = document.querySelector('.nav-close');
+const contents = document.querySelector('.contents-wrapper');
+
+headerBurg.addEventListener('click', () => {
+    mobileNav.classList.add('mod-showing');
+    // contents.classList.add('mod-moved');
+    body.classList.add('noscroll');
+});
+
+closeMobileNav.addEventListener('click', () => {
+    mobileNav.classList.remove('mod-showing');
+    // contents.classList.remove('mod-moved');
+    body.classList.remove('noscroll');
+});
