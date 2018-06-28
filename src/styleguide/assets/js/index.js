0b6a029905265e7a980def11606279d440a18fec
Hrag Chanchanian <hchanchanian@23andme.com>
2018-06-28 11:55:53 -0700

    transition for nav menu nailed

--- src/styleguide/assets/js/index.js
+++ src/styleguide/assets/js/index.js
@@ -3,15 +3,18 @@ const headerBurg = document.querySelector('.header-burg');
 const mobileNav = document.querySelector('.nav');
 const closeMobileNav = document.querySelector('.nav-close');
 const contents = document.querySelector('.contents-wrapper');
+const mobileHeader = document.querySelector('.header-wrapper');
 
 headerBurg.addEventListener('click', () => {
     mobileNav.classList.add('mod-showing');
-    // contents.classList.add('mod-moved');
+    contents.classList.add('mod-moved');
+    mobileHeader.classList.add('mod-moved');
     body.classList.add('noscroll');
 });
 
 closeMobileNav.addEventListener('click', () => {
     mobileNav.classList.remove('mod-showing');
-    // contents.classList.remove('mod-moved');
+    contents.classList.remove('mod-moved');
+    mobileHeader.classList.remove('mod-moved');
     body.classList.remove('noscroll');
 });
