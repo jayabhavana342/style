54488b819d3b3c145b010a89eaea633ef4db7a0b
Hrag Chanchanian <hchanchanian@23andme.com>
2018-07-13 17:27:41 -0700

    New design changes mostly in, working on drop-downs in nav

--- src/styleguide/assets/js/index.js
+++ src/styleguide/assets/js/index.js
@@ -4,17 +4,32 @@ const mobileNav = document.querySelector('.nav');
 const closeMobileNav = document.querySelector('.nav-close');
 const contents = document.querySelector('.contents-wrapper');
 const mobileHeader = document.querySelector('.header-wrapper');
+const overlay = document.querySelector('.nav-overlay');
 
-headerBurg.addEventListener('click', () => {
+function openNav() {
     mobileNav.classList.add('mod-showing');
     contents.classList.add('mod-moved');
     mobileHeader.classList.add('mod-moved');
     body.classList.add('noscroll');
-});
+    overlay.classList.add('mod-showing');
+}
 
-closeMobileNav.addEventListener('click', () => {
+function closeNav() {
     mobileNav.classList.remove('mod-showing');
     contents.classList.remove('mod-moved');
     mobileHeader.classList.remove('mod-moved');
     body.classList.remove('noscroll');
+    overlay.classList.remove('mod-showing');
+}
+
+headerBurg.addEventListener('click', () => {
+    openNav();
+});
+
+closeMobileNav.addEventListener('click', () => {
+    closeNav();
+});
+
+overlay.addEventListener('click', () => {
+    closeNav();
 });
