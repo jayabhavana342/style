be47ee09158df75acb1b5eb58dd180c578741070
Hrag Chanchanian <hchanchanian@23andme.com>
2018-07-16 16:46:55 -0700

    Most framing done, working on content layout

--- src/styleguide/assets/js/index.js
+++ src/styleguide/assets/js/index.js
@@ -1,13 +1,26 @@
 const body = document.getElementsByTagName('body')[0];
 const headerBurg = document.querySelector('.header-burg');
-const mobileNav = document.querySelector('.nav');
+const nav = document.querySelector('.nav');
 const closeMobileNav = document.querySelector('.nav-close');
 const contents = document.querySelector('.contents-wrapper');
 const mobileHeader = document.querySelector('.header-wrapper');
 const overlay = document.querySelector('.nav-overlay');
+const headerClose = document.querySelector('.header-close');
+
+function detectDesktopBurger() {
+    if (window.getComputedStyle(headerClose).order === '0') {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 function openNav() {
-    mobileNav.classList.add('mod-showing');
+    if (detectDesktopBurger()) {
+        headerBurg.classList.add('u-hide-for-desktop');
+        headerClose.classList.remove('u-hide-for-desktop');
+    }
+    nav.classList.add('mod-showing');
     contents.classList.add('mod-moved');
     mobileHeader.classList.add('mod-moved');
     body.classList.add('noscroll');
@@ -15,7 +28,11 @@ function openNav() {
 }
 
 function closeNav() {
-    mobileNav.classList.remove('mod-showing');
+    if (detectDesktopBurger()) {
+        headerBurg.classList.remove('u-hide-for-desktop');
+        headerClose.classList.add('u-hide-for-desktop');
+    }
+    nav.classList.remove('mod-showing');
     contents.classList.remove('mod-moved');
     mobileHeader.classList.remove('mod-moved');
     body.classList.remove('noscroll');
@@ -33,3 +50,17 @@ closeMobileNav.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     closeNav();
 });
+
+headerClose.addEventListener('click', () => {
+    closeNav();
+});
+
+// Handle resizes since the close button is entirely different for mobile vs desktop
+window.addEventListener('resize', function() {
+    if (nav.classList.contains('mod-showing')) {
+        if (detectDesktopBurger()) {
+            headerBurg.classList.add('u-hide-for-desktop');
+            headerClose.classList.remove('u-hide-for-desktop');
+        }
+    }
+}, true);
