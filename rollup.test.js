cd51e70321d205b767f3a3ff8d34d05bef8fd67c
Hrag Chanchanian <hchanchanian@23andme.com>
2018-07-19 15:27:01 -0700

    Some linting tweaks

--- rollup.test.js
+++ rollup.test.js
@@ -1,10 +1,10 @@
-import multiEntry from "rollup-plugin-multi-entry";
+import multiEntry from 'rollup-plugin-multi-entry'
 
 export default {
-    input: "src/components/**/test_*.js",
+    input: 'src/components/**/test_*.js',
     plugins: [multiEntry()],
     output: {
         file: '__test__/test.js',
         format: 'cjs'
     }
-};
+}
